38cdabc3be4cc9d3a28feaa0b966721fa476ec37
Zesheng Chen <chenz@pfw.edu>
2020-11-19 08:47:05 -0500

    Add main controller

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,11 @@
 // create a new express routes
 const express = require('express'),
-    router = express.Router()
+    router = express.Router(),
+    mainController = require('./controllers/main.controller')
 
 // export reroutes
 module.exports = router 
 
 // define routes
-router.get('/', (req, res) => {
-    res.send('Hello, CS372!')
-})
+router.get('/', mainController.showHome)
 
