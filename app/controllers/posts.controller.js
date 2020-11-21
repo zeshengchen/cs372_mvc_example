26014807d28f9f92053e83e2c86f06b2636e127d
Zesheng Chen <chenz@pfw.edu>
2020-11-21 09:15:17 -0500

    When seed db, remove all documents first

--- app/controllers/posts.controller.js
+++ app/controllers/posts.controller.js
@@ -7,15 +7,15 @@ module.exports = {
         // get all posts
 
         // return a view with data
-        res.render('pages/posts', { posts: posts });
+        res.render('pages/posts', { posts: posts })
     },
 
     // show a single post
     showSingle: (req, res) => {
         // get a single event
-        const post = { name: 'Homework', slug: 'homework', description: 'Homework is challenging!' };
+        const post = { name: 'Homework', slug: 'homework', description: 'Homework is challenging!' }
 
-        res.render('pages/single', { post: post });
+        res.render('pages/single', { post: post })
     },
 
     // seed our database
@@ -25,15 +25,17 @@ module.exports = {
             { name: 'Homework', description: 'Homework is challenging!' },
             { name: 'Lecture', description: 'Lecture is fun!' },
             { name: 'Project', description: 'Course Project is great!' }
-        ];
+        ]
 
         // use the Post model to insert/save
-        for (post of posts) {
-            var newPost = new Post(post);
-            newPost.save();
-        }
+        Post.deleteMany({}, () => {
+            for (let post of posts) {
+                var newPost = new Post(post)
+                newPost.save()
+            }
+        })
 
         // seeded!
-        res.send('Database seeded!');
+        res.send('Database seeded!')
     }
 }
